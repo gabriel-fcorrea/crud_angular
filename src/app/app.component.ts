63259604601379d011445a17875573b1205cc661
gabriel-fcorrea <gabrecorrea@gmail.com>
2025-05-20 12:05:57 -0300

    Projeto CRUD com Angular + Spring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    MatToolbarModule
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'assets' },
+  { path: 'assets',
+    loadChildren: () => import ('./assets/assets.module').then(m => m.AssetsModule)
+  }
+];
